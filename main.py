5a917b1d57899ed23e0bcb52752f1dce35bf26ea
alteroppa <fschweinfest@hotmail.com>
2018-07-18 23:27:01 +0200

    fix CNN

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import shutil
+import random
 import numpy as np
 import utils
 import os
+import random
 
-imageWidth, imageHeight = 340, 125
+imageWidth, imageHeight = 93, 75
+amountCroppedSplitDomino = 300 #len(os.listdir('screenshots/croppedDomino')) - 10
+amountCroppedSplitNoDomino = 400 #len(os.listdir('screenshots/croppedNoDomino')) - 10
+testAmount = int(amountCroppedSplitDomino / 5)
+trainAmount = int(amountCroppedSplitDomino / 5 * 4)
+
 
 def fetchImagesFromPool(train, test):
-    utils.fetchImagesFromPool(1, 1+train, 'resizedNoDominoStructure', 'train/noDomino')
-    utils.fetchImagesFromPool(1, 1+train, 'resizedDominoStructure', 'train/domino')
-    utils.fetchImagesFromPool(1+train, 1+train+test, 'resizedNoDominoStructure', 'test/noDomino')
-    utils.fetchImagesFromPool(1+train, 1+train+test, 'resizedDominoStructure', 'test/domino')
+    utils.fetchImagesFromPool(1, 1+train, 'screenshots/croppedNoDomino', 'screenshots/train/noDomino')
+    utils.fetchImagesFromPool(1, 1+train, 'screenshots/croppedDomino', 'screenshots/train/domino')
+    utils.fetchImagesFromPool(1+train, 1+train+test, 'screenshots/croppedNoDomino', 'screenshots/test/noDomino')
+    utils.fetchImagesFromPool(1+train, 1+train+test, 'screenshots/croppedDomino', 'screenshots/test/domino')
 
 def emptyTrainAndTestFolders():
-    utils.emptyFolder('train/noDomino')
-    utils.emptyFolder('train/domino')
-    utils.emptyFolder('test/noDomino')
-    utils.emptyFolder('test/domino')
+    utils.emptyFolder('screenshots/train/noDomino')
+    utils.emptyFolder('screenshots/train/domino')
+    utils.emptyFolder('screenshots/test/noDomino')
+    utils.emptyFolder('screenshots/test/domino')
 
-def trainModel1(train, epochs=5):
+def trainModel1(train, epochs=5, folds=5):
     print('Training model with', epochs, 'epochs...')
     imgWidth, imgHeight = imageWidth, imageHeight
 
-    trainDataDir = 'train'
-    testDataDir = 'test'
+    trainDataDir = 'screenshots/trainSplit'
+    testDataDir = 'screenshots/testSplit'
 
-    trainSamples = 800
-    testSamples = 200
+    trainSamples = trainAmount
+    testSamples = testAmount
 
     epochs = epochs #should be more like 50
     batchSize = 32
@@ -40,87 +48,85 @@ def trainModel1(train, epochs=5):
     else:
         input_shape = (imgWidth, imgHeight, 3)
 
+
+    for i in range(folds):
+        splitPrepare(trainAmount, testAmount)
+
+        model = getModel(input_shape)
+
+        trainDataGen = ImageDataGenerator(rescale=1./255)
+        testDataGen = ImageDataGenerator(rescale=1./255)
+
+        trainingSet = trainDataGen.flow_from_directory(trainDataDir, target_size=(imgWidth, imgHeight),
+            batch_size=batchSize,
+            class_mode='binary')
+
+        testSet = testDataGen.flow_from_directory(
+            testDataDir,
+            target_size=(imgWidth, imgHeight),
+            batch_size=batchSize,
+            class_mode='binary')
+
+        ''' Returns:
+        A DirectoryIterator yielding tuples of(cross, y) where cross is a numpy array containing a batch of images with shape(batch_size, *target_size, channels) and y is a numpy array of corresponding labels.
+        
+    
+        print(len(trainingSet)) # anzahl bilder / batchSize
+        print(len(trainingSet[0])) # tuple
+        print(len(trainingSet[1])) # tuple
+        print(len(trainingSet[2])) # tuple
+    
+        print(len(trainingSet[0][0])) # tuple 0, numpy array 0 (numpy array containing a batch of images with shape(batch_size, *target_size, channels)) = batch of 32 images
+        print(trainingSet[0][0][0].shape)
+        print(len(trainingSet[0][0][0][0])) # erstes image
+    
+        print(trainingSet[0][0][0][0]) # erstes image
+    
+        #print(trainingSet[0][0])
+        #print(trainingSet[1][0])
+        '''
+
+        if train:
+            print('training...')
+            output = model.fit_generator(
+                trainingSet,
+                steps_per_epoch=trainSamples // batchSize,
+                epochs=epochs,
+                validation_data=testSet,
+                validation_steps=testSamples // batchSize,
+                verbose=2)
+
+            model.save_weights('firstTry.h5')
+        else:
+            model.load_weights('firstTry.h5')
+
+
+        validateImages(model, trainingSet)
+
+
+def getModel(input_shape):
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    '''
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))'''
-
     model.add(Flatten())
-    model.add(Dense(units = 128, activation ='relu'))
-    #model.add(Dropout(0.5))
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(units=1, activation='sigmoid'))
-
-
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
-
-    trainDataGen = ImageDataGenerator(rescale=1./255)
-    testDataGen = ImageDataGenerator(rescale=1./255)
-
-    trainingSet = trainDataGen.flow_from_directory(trainDataDir, target_size=(imgWidth, imgHeight),
-        batch_size=batchSize,
-        class_mode='binary')
-
-    testSet = testDataGen.flow_from_directory(
-        testDataDir,
-        target_size=(imgWidth, imgHeight),
-        batch_size=batchSize,
-        class_mode='binary')
-
-    ''' Returns:
-    A DirectoryIterator yielding tuples of(x, y) where x is a numpy array containing a batch of images with shape(batch_size, *target_size, channels) and y is a numpy array of corresponding labels.
-    '''
-
-    print(len(trainingSet)) # anzahl bilder / batchSize
-    print(len(trainingSet[0])) # tuple
-    print(len(trainingSet[1])) # tuple
-    print(len(trainingSet[2])) # tuple
-
-    print(len(trainingSet[0][0])) # tuple 0, numpy array 0 (numpy array containing a batch of images with shape(batch_size, *target_size, channels)) = batch of 32 images
-    print(trainingSet[0][0][0].shape)
-    print(len(trainingSet[0][0][0][0])) # erstes image
-
-    print(trainingSet[0][0][0][0]) # erstes image
-
-    #print(trainingSet[0][0])
-    #print(trainingSet[1][0])
-
-    if train:
-        model.fit_generator(
-            trainingSet,
-            steps_per_epoch=trainSamples // batchSize,
-            epochs=epochs,
-            validation_data=testSet,
-            validation_steps=testSamples // batchSize,
-            verbose=1)
-
-        model.save_weights('firstTry.h5')
-    else:
-        model.load_weights('firstTry.h5')
-
-
-    validateImages(model, trainingSet)
-
-def trainModel2():
-    return
+    return model
 
 
 def validateImages(model, trainingSet):
     print(trainingSet.class_indices)
-    for file in os.listdir('validationFolder'):
+    for file in os.listdir('screenshots/validationFolder'):
         if not file.startswith('.'):
-            utils.resizeImagesInFolder((imageWidth, imageHeight), 'validationFolder')
-            filePlusDir = os.path.join('validationFolder', file)
+            filePlusDir = os.path.join('screenshots/validationFolder', file)
             testImage = image.load_img(filePlusDir, target_size=(imageWidth, imageHeight))
             testImage = image.img_to_array(testImage)
             testImage = np.expand_dims(testImage, axis=0)
@@ -133,16 +139,86 @@ def validateImages(model, trainingSet):
             print(file + ': ' + prediction)
             print(result)
 
+
+def prepare():
+    emptyTrainAndTestFolders()
+    utils.emptyFolder('screenshots/resizedDomino')
+    utils.emptyFolder('screenshots/resizedNoDomino')
+    utils.emptyFolder('screenshots/originalDomino')
+    utils.emptyFolder('screenshots/originalNoDomino')
+    utils.emptyFolder('screenshots/validationFolder')
+    utils.emptyFolder('screenshots/croppedDomino')
+    utils.emptyFolder('screenshots/croppedNoDomino')
+    utils.getCoordinatesFile()
+    utils.fetchImagesFromSourceToOriginalFolders()
+    utils.splitAndCropImagesDominoScreenshots('screenshots/originalDomino')
+    utils.splitAndCropImagesNoDominoScreenshots('screenshots/originalNoDomino')
+
+
+    # copies the test images into the folders
+    for file in os.listdir('screenshots/croppedDomino')[:testAmount]:
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/test/domino', file)
+            shutil.copy(os.path.join('screenshots/croppedDomino', file), filePathToSave)
+    for file in os.listdir('screenshots/croppedNoDomino')[:testAmount]:
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/test/noDomino', file)
+            shutil.copy(os.path.join('screenshots/croppedNoDomino', file), filePathToSave)
+    # copies the train images into the folders
+    for file in os.listdir('screenshots/croppedDomino')[testAmount:amountCroppedSplitDomino]:
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/train/Domino', file)
+            shutil.copy(os.path.join('screenshots/croppedDomino', file), filePathToSave)
+    for file in os.listdir('screenshots/croppedNoDomino')[testAmount:amountCroppedSplitNoDomino]:
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/train/noDomino', file)
+            shutil.copy(os.path.join('screenshots/croppedNoDomino', file), filePathToSave)
+    # copies the last 10 pictures of each class into the validation folder
+    #for file in random.sample(os.listdir('screenshots/croppedDomino'), 10):
+    for file in random.sample(os.listdir('screenshots/croppedDomino')[:30], 10):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/validationFolder/' , 'domino' + file)
+            shutil.copy(os.path.join('screenshots/croppedDomino', file), filePathToSave)
+    #for file in random.sample(os.listdir('screenshots/croppedNoDomino'), 10):
+    for file in random.sample(os.listdir('screenshots/croppedNoDomino')[:30], 10):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/validationFolder/', 'noDomino' + file)
+            shutil.copy(os.path.join('screenshots/croppedNoDomino', file), filePathToSave)
+    #utils.resizeImagesInFolder((imageWidth, imageHeight), 'screenshots/validationFolder')
+
+    return (testAmount, trainAmount)
+
+
+
+
+def splitPrepare(trainSampleSize, testSampleSize):
+    utils.emptyFolder('screenshots/testSplit/domino')
+    utils.emptyFolder('screenshots/testSplit/noDomino')
+    utils.emptyFolder('screenshots/trainSplit/domino')
+    utils.emptyFolder('screenshots/trainSplit/noDomino')
+
+    for file in random.sample(os.listdir('screenshots/croppedDomino'), testSampleSize):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/testSplit/domino', file)
+            shutil.copy(os.path.join('screenshots/croppedDomino', file), filePathToSave)
+    for file in random.sample(os.listdir('screenshots/croppedNoDomino'), testSampleSize):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/testSplit/noDomino', file)
+            shutil.copy(os.path.join('screenshots/croppedNoDomino', file), filePathToSave)
+
+    for file in random.sample(os.listdir('screenshots/croppedDomino'), trainSampleSize):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/trainSplit/domino', file)
+            shutil.copy(os.path.join('screenshots/croppedDomino', file), filePathToSave)
+    for file in random.sample(os.listdir('screenshots/croppedNoDomino'), trainSampleSize):
+        if file.endswith('.png'):
+            filePathToSave = os.path.join('screenshots/trainSplit/noDomino', file)
+            shutil.copy(os.path.join('screenshots/croppedNoDomino', file), filePathToSave)
+
+
 if __name__ == "__main__":
-    #emptyTrainAndTestFolders()
-    #utils.emptyFolder('resizedDominoStructure')
-    #utils.emptyFolder('resizedNoDominoStructure')
-    #utils.resizeImages((imageWidth, imageHeight)) # has to be a tuple, has to maintain aspect ratio of 1,75
-    #fetchImagesFromPool(400, 100)
-    trainModel1(train=False, epochs=20)
-    #trainModel2()
 
+    #prepare()
+    trainModel1(train=True, epochs=1, folds=5)
 
-
-    #print('domino:',utils.getFolderSize('originalDominoStructure'))
-    #print('no domino:',utils.getFolderSize('originalNoDominoStructure'))
+ 
--- utils.py
+++ utils.py
@@ -3,20 +3,39 @@ import os
 import shutil
 
 def resizeImages(size):
-    for file in os.listdir('originalDominoStructure'):
-        if not file.startswith('.'):
-            image = Image.open('originalDominoStructure/' + file)
+    print('resizing images in original folders to size ' + size + ' ...')
+    for file in os.listdir('screenshots/originalDomino'):
+        if file.endswith('.png'):
+            image = Image.open('screenshots/originalDomino/' + file)
             newImage = image.resize(size)
-            newImage.save('resizedDominoStructure/' + file)
-    for file in os.listdir('originalNoDominoStructure'):
-        if not file.startswith('.'):
-            image = Image.open('originalNoDominoStructure/' + file)
+            newImage.save('screenshots/resizedDomino/' + file)
+    for file in os.listdir('screenshots/originalNoDomino'):
+        if file.endswith('.png'):
+            image = Image.open('screenshots/originalNoDomino/' + file)
             newImage = image.resize(size)
-            newImage.save('resizedNoDominoStructure/' + file)
+            newImage.save('screenshots/resizedNoDomino/' + file)
+
+def getCoordinatesFile():
+    coordinatesSrc = '../aiBirds/levelGenerator/generatedLevels/dominoCoordinates.txt'
+    coordinatesGoal = 'screenshots/coordinates.txt'
+    shutil.copy(coordinatesSrc, coordinatesGoal)
+
+
+def fetchImagesFromSourceToOriginalFolders():
+    print('fetching images from source (aibirds/abV1.32/screenshots) to screenshots/original[No]Domino ...')
+    srcfolderDomino = '../aiBirds/abV1.32/screenshots/dominoStructure'
+    goalfolderDomino = 'screenshots/originalDomino'
+    amountOfPicsDomino = len(os.listdir(srcfolderDomino))
+    fetchImagesFromPool(0, amountOfPicsDomino, srcfolderDomino, goalfolderDomino)
+    srcfolderNoDomino = '../aiBirds/abV1.32/screenshots/noDominoStructure'
+    goalfolderNoDomino = 'screenshots/originalNoDomino'
+    amountOfPicsNoDomino = len(os.listdir(srcfolderNoDomino))
+    fetchImagesFromPool(0, amountOfPicsNoDomino, srcfolderNoDomino, goalfolderNoDomino)
 
 def resizeImagesInFolder(size, folderName):
+    print('resizing images in folder ' + folderName + ' to size ' + str(size) + ' ...')
     for file in os.listdir(folderName):
-        if not file.startswith('.'):
+        if file.endswith('.png'):
             image = Image.open(folderName + '/' + file)
             newImage = image.resize(size)
             newImage.save(folderName +'/' + file)
@@ -25,21 +44,110 @@ def getFolderSize(folderName):
     return(len(os.listdir(folderName)))
 
 def fetchImagesFromPool(amountFrom, amountTo, folderFrom, folderTo):
+    print('fetching images from pool from folder ' + folderFrom + ' to folder ' + folderTo + ' from amount ' + str(amountFrom) + ' to amount ' + str(amountTo) + ' ...')
     srcFiles = os.listdir(folderFrom)[amountFrom:amountTo]
+    amount = 0
     for fileName in srcFiles:
-        if not fileName.startswith('.'):
+        if fileName.endswith('.png'):
             fullFileNameFrom = os.path.join(folderFrom, fileName)
             fullFileNameTo = os.path.join(folderTo, fileName)
             shutil.copy(fullFileNameFrom, fullFileNameTo)
+            amount = amount + 1
+    print('fetched ' + str(amount) + ' pictures from ' + folderFrom + ' and saved them at ' + folderTo)
+
+
 
 def emptyFolder(folderName):
+    amount = len(os.listdir(folderName))
     for file in os.listdir(folderName):
         os.remove(os.path.join(folderName,file))
+    print('emptied folder ' + folderName + ' with ' + str(amount) + 'files.')
 
 def cropImagesInFolder(folderName):
+    print('cropping images in folder ' + folderName + '...')
     for file in os.listdir(folderName):
-        if not file.startswith('.'):
+        if file.endswith('.png'):
             image = Image.open(folderName + '/' + file)
             width, height = image.size
             newImage = image.crop((0, 40, width-10, 165))
             newImage.save(folderName + '/' + file)
+
+def splitAndCropImagesDominoScreenshots(folderToRead):
+    print('splitting and cropping images in folder ' + folderToRead + '...')
+    with open('screenshots/coordinates.txt') as coordsFromFile:
+        coordinates = coordsFromFile.readlines()
+    cutPixelsUntilSlingshot = 52
+    cutPixelsFromEnd = 40
+
+    for file in os.listdir(folderToRead):
+        if file.endswith('.png'):
+            levelNumber = file[5:-4]
+            #structureX = (float(coordinates[levelCounter].split(';')[1])) * 5 + 14 - cutPixelsUntilSlingshot
+            structureX = (int(coordinates[int(levelNumber) - 1].split(';')[1])) * 5 + 14 - cutPixelsUntilSlingshot
+            #structureY =  #coordinates[int(levelNumber) -1].split(';')[2]
+
+            image = Image.open(folderToRead + '/' + file)
+            originalWidth, originalHeight = image.size
+            newImage = image.crop((cutPixelsUntilSlingshot, 90, originalWidth - cutPixelsFromEnd, 390))
+            #newImage.save('screenshots/croppedDomino/croppedOriginal' + file[:-4] + '_' + str(i) + '.png')
+            newWidth, newHeight = newImage.size
+
+            quarterWidth = newWidth/4
+            halfHeight = newHeight/2
+
+            screenshotWithDominoDone = False
+            for j in range(4):
+                x1 = (j) * quarterWidth
+                x2 = (j+1) * quarterWidth
+
+                # top half cut in 4 quarters
+                newImageTopHalf = newImage.crop((x1, 0, x2, halfHeight))
+
+                # for now: don't save top half, because there is no point anyway - it's just blue sky. Enable optionally.
+                #newImageTopHalf.save('screenshots/croppedNoDomino/croppedTop' + file[:-4] + '_' + str(j+1) + '.png')
+
+                # bottom half cut in 4 quarters
+                newImageBottomHalf = newImage.crop((x1, halfHeight, x2, newHeight))
+                if structureX > int((x1 + quarterWidth/8)) and structureX < int((x2 - quarterWidth/8)):
+                    #print('structurex: ', structureX)
+                    #print('x1: ', (x1 + quarterWidth/9))
+                    #print('x2: ', (x2 - quarterWidth/9))
+                    newImageBottomHalf.save('screenshots/croppedDomino/croppedBot' + file[:-4] + '_' + str(j+1) + '.png')
+                    screenshotWithDominoDone = True
+                else:
+                    newImageBottomHalf.save('screenshots/croppedNoDomino/croppedBot' + file[:-4] + '_' + str(j+1) + '.png')
+
+            # if no screenshot with domino done yet, do one extra
+            '''if screenshotWithDominoDone == False:
+                definitiveDominoStructure = newImage.crop((structureX - quarterWidth/2, halfHeight, structureX + quarterWidth/2, newHeight))
+                definitiveDominoStructure.save(
+                    'screenshots/croppedDomino/croppedBot' + file[:-4] + '_extra.png')'''
+
+def splitAndCropImagesNoDominoScreenshots(folderToRead):
+    print('splitting and cropping images in folder ' + folderToRead + '...')
+    cutPixelsUntilSlingshot = 52
+    cutPixelsFromEnd = 40
+    levelCounter = 0
+    for file in os.listdir(folderToRead):
+        if file.endswith('.png'):
+            image = Image.open(folderToRead + '/' + file)
+            originalWidth, originalHeight = image.size
+            newImage = image.crop((cutPixelsUntilSlingshot, 90, originalWidth - cutPixelsFromEnd, 390))
+            # newImage.save('screenshots/croppedDomino/croppedOriginal' + file[:-4] + '_' + str(i) + '.png')
+            newWidth, newHeight = newImage.size
+
+            quarterWidth = newWidth / 4
+            halfHeight = newHeight / 2
+            for j in range(4):
+                x1 = (j) * quarterWidth
+                x2 = (j + 1) * quarterWidth
+
+                # top half cut in 4 quarters
+                newImageTopHalf = newImage.crop((x1, 0, x2, halfHeight))
+
+                # for now: don't save top half, because there is no point anyway - it's just blue sky. Enable optionally.
+                # newImageTopHalf.save('screenshots/croppedNoDomino/croppedTop' + file[:-4] + '_' + str(j+1) + '.png')
+
+                # bottom half cut in 4 quarters
+                newImageBottomHalf = newImage.crop((x1, halfHeight, x2, newHeight))
+                newImageBottomHalf.save('screenshots/croppedNoDomino/croppedBot' + file[:-4] + '_' + str(j + 1) + '.png')
